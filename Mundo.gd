d4545906072ec94451230d5164d0caed5cd6950b
Joshua <ccquicenol@uqvirtual.edu.co>
2021-07-14 13:23:14 -0500

    Mundo Realizado
    Se crean botellas aleatorias y ya se puede agarrar botellas

---
+++ Mundo.gd
@@ -0,0 +1,27 @@
+extends Node
+export (PackedScene) var Residuo
+var score
+
+func _ready():
+	randomize()
+
+func nuevo_juego():
+	score = 0
+	$InicioTimer.start() 
+
+func _on_InicioTimer_timeout():
+	$ResiduoTimer.start()
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	#score += 1
+	pass
+
+
+func _on_ResiduoTimer_timeout():
+	$Camino/ResiduoPosicion.set_offset(randi())
+	var R = Residuo.instance()
+	add_child(R)
+	R.position = $Camino/ResiduoPosicion.position
+
+	
