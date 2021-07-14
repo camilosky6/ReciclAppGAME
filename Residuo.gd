7313a4db693ee799c5da7bddb3acc0a24224ffa5
Joshua <ccquicenol@uqvirtual.edu.co>
2021-07-14 13:24:39 -0500

    Carpeta movida
    Arreglar pollo, quitar pitillo, y poner los nombres asi:
    obj[categoria]#, ejmpl: objPapel2

--- Residuo.gd
+++ Residuo.gd
@@ -2,10 +2,27 @@ extends RigidBody2D
 
 export (int) var velocidad_min
 export (int) var velocidad_max
+var selected = false
 var tipo_residuo = ["carton", "papel", "otro"]
 
 func _ready():
-	$AnimatedSprite.animation = tipo_residuo[randi() % tipo_residuo.size()]
+	#$AnimatedSprite.animation = tipo_residuo[randi() % tipo_residuo.size()]
+	set_process(true)
+
+func _process(delta):
+	if selected:
+		seguirMouse()
+
+func seguirMouse():
+	self.position = get_global_mouse_position()
 
 func _on_Visibilidad_screen_exited():
 	queue_free() #Elimina el Residuo cuando deja de ser visible
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event.is_action_pressed("click"):
+		selected = true
+		print("se hizo cliccckkkk")
+	if event.is_action_released("click"):
+		print("se ha soltado")
+		selected = false
