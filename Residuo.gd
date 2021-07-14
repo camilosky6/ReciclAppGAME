295855936cb46537f7dcf309c0b4c97684948a38
Joshua <ccquicenol@uqvirtual.edu.co>
2021-07-14 17:39:37 -0500

    Bordeado del Mundo y Residuo Random
    Se borde el muro para que no salgan, se agrego colisión con los residuos y se agrego el indicio de una mesa que los retiene

--- Residuo.gd
+++ Residuo.gd
@@ -3,11 +3,24 @@ extends RigidBody2D
 export (int) var velocidad_min
 export (int) var velocidad_max
 var selected = false
-var tipo_residuo = ["carton", "papel", "otro"]
+
+#Categorias Resiclaje
+var tipo_residuo = ["obj","objPlastico1"]
+var obj = ["obj","objPlastico1"]
 
 func _ready():
-	#$AnimatedSprite.animation = tipo_residuo[randi() % tipo_residuo.size()]
+	$AnimatedSprite.animation = tipo_residuo[randi() % tipo_residuo.size()]
+	crearObjeto($AnimatedSprite.animation)
 	set_process(true)
+
+func crearObjeto(sprite):
+	match sprite:
+		"obj": 
+			$CollisionShape2D.scale.x = 0.5
+			$CollisionShape2D.scale.y = 0.5
+		"objPlastico1":
+			$CollisionShape2D.scale.x = 1
+			$CollisionShape2D.scale.y = 1
 
 func _process(delta):
 	if selected:
@@ -22,7 +35,6 @@ func _on_Visibilidad_screen_exited():
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("click"):
 		selected = true
-		print("se hizo cliccckkkk")
 	if event.is_action_released("click"):
-		print("se ha soltado")
+		self.linear_velocity = Vector2(0,0)
 		selected = false
